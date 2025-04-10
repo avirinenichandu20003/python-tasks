e419b319a97d5b7a0849816f43ad5a9398f63098
AVIRINENI MOHITH SAI CHANDU <avirineni.net@gmail.com>
2025-04-10 19:29:56 +0530

    Update A4_Task1.py
    Task 1: Read a File and Handle Errors 
    Problem Statement:  Write a Python program that:
    1.   Opens and reads a text file named sample.txt.
    2.   Prints its content line by line.
    3.   Handles errors gracefully if the file does not exist.

--- A4_Task1.py
+++ A4_Task1.py
@@ -1,11 +1,12 @@
 try:
-    file=open(".venv/sample.txt","r")
+    b=".venv/sample.txt"
+    file=open(b,"r")
     a=(file.readlines())
     for i in range (len(a)):
         print(a[i],end="")
     file.close()
 except FileNotFoundError:
-    print("Correct the file")
+    print("This File",b,"Not Found")
 finally:
     print("\nFile reading attempt complete.")
 

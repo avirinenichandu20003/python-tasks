f43a3b129a5e02c6d328f57e09a361415d921a39
AVIRINENI MOHITH SAI CHANDU <avirineni.net@gmail.com>
2025-04-10 12:39:43 +0530

    Create A3_Task2.py
    Problem Statement: Write a Python program that:
    1.   Asks the user for a number as input.
    2.   Uses the math module to calculate the:
    o   Square root of the number
    o   Natural logarithm (log base e) of the number
    o   Sine of the number (in radians)
    3.   Displays the calculated results.

---
+++ A3_Task2.py
@@ -0,0 +1,8 @@
+import math
+def mat(a):
+    print("Square root:", math.sqrt(a))
+    print("Natural log:", math.log(a))
+    print("Sine (in radians):", math.sin(a))
+
+a = float(input("Enter a number: "))
+mat(a)
